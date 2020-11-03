22e5899499440d4533f87b70b5f1dc5d3661f38e
novb0127 <nobuyukiyamamoto@gmail.com>
2020-11-03 22:59:38 +0900

    delete posts teble

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 2020_10_31_070217) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
